fbcc42b20dcc9792a218e7698a54d16b8f5ee7e7
kamil.sadaj <kamilsadaj@gmail.com>
2026-01-29 20:48:12 +0100

    #4 Convert to multi module

---
+++ backend/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.kotest)
+    alias(libs.plugins.versions)
+}
+
+group = "pl.taurus"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+dependencies {
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.data.jdbc)
+    implementation(libs.spring.boot.starter.jdbc)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.flyway)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.spring.modulith.starter.core)
+    implementation(libs.spring.modulith.starter.jdbc)
+    implementation(libs.kotlin.logging.jvm)
+
+    runtimeOnly(libs.h2)
+    runtimeOnly(libs.spring.modulith.actuator)
+    runtimeOnly(libs.spring.modulith.observability)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.boot.starter.webmvc.test)
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.spring.modulith.starter.test)
+    testImplementation(libs.kotest.framework.engine)
+    testImplementation(libs.kotest.runner.junit5)
+    testImplementation(libs.kotest.extensions.spring)
+    testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.modulith:spring-modulith-bom:${libs.versions.spring.modulith.get()}")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+spotless {
+    kotlin {
+        ktlint(libs.versions.ktlint.get())
+    }
+    kotlinGradle {
+        ktlint(libs.versions.ktlint.get())
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
